68cf4d8ff5dded3af799f4a62ee6fdeea4ef374d
xzhan211 <xiaoyangzhang2015@gmail.com>
2020-05-04 01:09:09 -0400

    use cron to schedule spider apps

--- tutorial/AutoNoticeSpider.py
+++ tutorial/AutoNoticeSpider.py
@@ -5,14 +5,15 @@ Version: 1.0
 '''
 
 import subprocess
-from multiprocessing import Process
-import time
+# from multiprocessing import Process
+# import time
 
 # delete the old one
-# terminal $ rm quotes.json 
+# terminal $ rm quotes.json
 deleteCmd = ['rm', 'quotes.json']
 
 # terminal $ scrapy crawl quotes_EL -o quotes.json -L WARN
+# runSpiderCmd = ['scrapy', 'crawl', 'quotes_EL', '-o', '/Users/xiaoyangzhang/git_base/Spider/tutorial/quotes.json', '-L', 'WARN']
 runSpiderCmd = ['scrapy', 'crawl', 'quotes_EL', '-o', 'quotes.json', '-L', 'WARN']
 
 # terminal $ python sender.py
@@ -22,38 +23,41 @@ def deleteDoc():
 	subprocess.check_call(deleteCmd)
 
 def getData():
-	subprocess.check_call(runSpiderCmd)	
+	subprocess.check_call(runSpiderCmd)
 
 def sendEmail():
-	subprocess.check_call(sendEmailCmd)	
+	subprocess.check_call(sendEmailCmd)
 
 def mySpider():
 	try:
 		deleteDoc()
-		# print("quotes.json is deleted successful!")
-	except:
-		print("file doesn't exist, don't worry!")
+	except Exception as e:
+		print("file doesn't exist, don't worry --- ", e)
 
 	try:
 		getData()
-		# print("Grab data. Done!")
+	except Exception as e:
+		print("error in getData() --- ", e)
+
+	try:
 		sendEmail()
-		# print("Send email. Done!")
-	except:
-		print("error in getData() or sendEmail()")
+	except Exception as e:
+		print("error in sendEmail() --- ", e)
 
 
 def main():
+	mySpider()
+	# way 1
 	# it is a dead loop, press ctrl c to exit!
-	while True:
-		p = Process(target=mySpider)
-		p.start()
-		p.join()
-		time.sleep(43200)
+	# while True:
+	# 	p = Process(target=mySpider)
+	# 	p.start()
+	# 	p.join()
+	# 	time.sleep(43200)
+		# time.sleep(10)
 
 	# run process in background , please add '&'
-	# kill process $ kill pid 
-
+	# kill process $ kill pid
 
 if __name__ == "__main__":
 	main()
---
+++ tutorial/start_spider.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd /Users/xiaoyangzhang/git_base/Spider/tutorial
+#PATH=$PATH:/Users/xiaoyangzhang/anaconda3/bin
+#export PATH
+python AutoNoticeSpider.py
